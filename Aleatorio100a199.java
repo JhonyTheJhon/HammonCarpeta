8f53bb8fe76d2d8e2ec49a84cd77d9fa185a4910
JhonyTheJhon <202213470a@utea.edu.pe>
2026-05-31 19:40:48 -0500

    Update Aleatorio100a199.java

--- Aleatorio100a199.java
+++ Aleatorio100a199.java
@@ -1,11 +1,30 @@
 public class Aleatorio100a199 {
     public static void main(String[] args) {
         
-        System.out.println("Programa que muestra 50 números aleatorios de 100 a 199: ");
+        int Maximo = 100;
+	    int Minimo = 199;
+	    int Sumador = 0;
+	    
+		System.out.println("Programa que muestra 50 números aleatorios de 100 a 199: ");
 
         for (int i = 1 ; i < 51 ; i++) {
-            System.out.println((int) (Math.random()*100 + 100));
-            System.out.println("");
+            int Numero = (int) (Math.random()*100 + 100);
+            
+            System.out.print(Numero + " ");
+            
+            Sumador = Sumador + Numero;
+            
+            if (Numero > Maximo) {
+                Maximo = Numero;
+            }
+
+            if (Numero < Minimo) {
+                Minimo = Numero;
+            }
         }
+        
+        System.out.println("\nEl Número Máximo es: " + Maximo);
+        System.out.println("El Número Mínimo es: " + Minimo);
+        System.out.println("El Promedio de estos Números es: " + (Sumador / 50));
     }
 }
